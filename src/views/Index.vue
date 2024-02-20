4003a2c2d85867efdfcb6cd1335863a5f595ed4a
7ashs <dupaquier.jules@gmail.com>
2024-02-21 00:46:40 +0100

    First Version Video Descriptions

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,22 +1,33 @@
 <script>
 </script>
 <template>
-    <div>
-        <video className="h-full w-full rounded-lg" autoPlay muted>
+    <div class="video-container relative">
+        <video class="h-full w-full rounded-lg" autoplay muted loop>
             <source src="https://docs.material-tailwind.com/demo.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
+        <div class="overlay absolute top-1/2 left-0 transform -translate-y-1/2 p-4 text-white">
+            <h2 class="text-5xl text-noir font-black font-Helvetica">JEUNE AMBITIEUX, JEUNE DYNAMIQUE.</h2>
+            <p class="mt-2 text-xl text-noir font-normal font-Helvetica">Bienvenue sur julesdupaquier.fr ! Je m’appelle Jules, j’ai 20 ans, et à travers ce site, vous entrez dans mon univers.</p>
+        </div>
+        <div class="chevron absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-4">
+            <a href="#section-suivante">
+                <svg class="h-12 w-12 text-white animate-bounce" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+                </svg>
+            </a>
+        </div>
     </div>
     <div class="container">
         <div class="marquee">
             <p class="text-gris font-Helvetica text-marquee">
             <marquee behavior="scroll" direction="left" scrollamount="30">
-            Nice to meet you, I'm Jules.
+            Nice to meet you, I'm Jules.・Nice to meet you, I'm Jules.・Nice to meet you, I'm Jules.・Nice to meet you, I'm Jules.・Nice to meet you, I'm Jules.・Nice to meet you, I'm Jules.
             </marquee>
         </p>
         <p class="text-blanc font-Helvetica text-marquee">
             <marquee behavior="scroll" direction="right" scrollamount="30">
-            Enchanté, moi c'est Jules.
+            Enchanté, moi c'est Jules.・Enchanté, moi c'est Jules.・Enchanté, moi c'est Jules.・Enchanté, moi c'est Jules.・Enchanté, moi c'est Jules.・Enchanté, moi c'est Jules.・Enchanté, moi c'est Jules.
             </marquee>
         </p>
         </div>
@@ -31,8 +42,8 @@
             </p>
         </div>
     </div>
-    <div class="m-96">
-        <p>test</p>
+    <div class="bg-blanc">
+        <h1 class="text-noir font-Helvetica">Alors, on commence par quoi ?</h1>
     </div>
 </template>
 
