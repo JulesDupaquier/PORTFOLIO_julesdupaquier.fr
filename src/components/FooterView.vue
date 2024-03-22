34a01dfdcb8c30cc083071bb24f9afe123a1c1b3
7ashs <dupaquier.jules@gmail.com>
2024-03-23 00:31:25 +0100

    Update Footer

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,25 +1,21 @@
 <template>
-    <div class="bg-noir pt-10">
+    <div class="bg-blanc pt-10">
         <div class="ml-8 flex items-center">
             <img src="/icones/triple7-light.svg" alt="Triple7-LogoSansBorder" class="w-24 h-auto">
         </div>
-        <div class="flex flex-col md:flex-row justify-center text-center space-y-4 md:space-y-0 md:space-x-16 mt-4">
-            <p class="linkf">Politique de confidentialité</p>
-            <p class="linkf">Mentions légales</p>
+        <div class="flex flex-col md:flex-row text-center mt-4">
+            <a href=""></a><p class="linkf text-noir">Télécharger mon CV</p>
         </div>
         <div class="justify-center text-center">
-            <hr class="ml-16 mr-16 rounded md:my-10 border-blanc">
+            <hr class="ml-16 mr-16 rounded md:my-10 border-noir">
         </div>
-        <div class="flex justify-center pt-6">
+        <div class="flex justify-center pt-6 text-noir">
             <a href="https://www.linkedin.com/in/julesdupaquier/" class="mx-2"><img src="icones/linkedin.svg" alt="Linkedin"/></a>
             <a href="https://www.instagram.com/triple7fr/" class="mx-2"><img src="icones/instagram.svg" alt="Instagram"/></a>
             <a href="mailto:contact@julesdupaquier.fr" class="mx-2"><img src="icones/mail.svg" alt="Mail"/></a>
             <a href="tel:+33772168094" class="mx-2"><img src="icones/telephone.svg" alt="Telephone"/></a>
         </div>
-        <h6 class="text-blanc text-center font-Helvetica pt-6 pb-4">© 2024 Jules Dupaquier. Tous droits réservés.</h6>
-    </div>
-    <div>
-      <p>Remonter tout en haut !</p>
+        <h6 class="text-noir text-center font-Helvetica pt-6 pb-4">© 2024 Jules Dupaquier. Tous droits réservés.</h6>
     </div>
 </template>
 
@@ -30,7 +26,7 @@
 .linkf {
   position: relative;
   display: inline-block;
-  color: #FFFFFF;
+  color: #101010;
   transition: color 0.3s; /* Transition de couleur de texte de 0.3 seconde */
 }
 
@@ -40,7 +36,7 @@
   position: absolute;
   width: 0;
   height: 1px; /* Ajustez l'épaisseur de la ligne */
-  background-color: #FFFFFF; /* Couleur de la ligne */
+  background-color: #101010; /* Couleur de la ligne */
   transition: width 0.3s, left 0.3s; /* Transition de largeur de la ligne et de la position à gauche de 0.3 seconde */
 }
 
@@ -55,7 +51,7 @@
 }
 
 .linkf:hover {
-  color: #FFFFFF; /* Texte rouge lors du survol */
+  color: #101010; /* Texte rouge lors du survol */
 }
 
 .linkf:hover::before,
